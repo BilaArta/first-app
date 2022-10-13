c12e12fbaf63c420b43e755a015a55db4d332e14
BilaArta <bilaarta@gmail.com>
2022-10-13 15:28:41 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 //import logo from './logo.svg';
 import './App.css';
 import Table from "./Components/Table";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="container">
-      {/* <header className="App-header">
-      </header> */}
-      <h1>Hello World</h1>
-      <Table/>
-    </div>
-  );
+class App extends Component {
+    state = {
+     characters : [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      }
+    ]
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+  
+  render() {
+    const { characters } = this.state
+  
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+      </div>
+    )
+  }
 }
 
 export default App;
